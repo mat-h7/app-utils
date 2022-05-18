a1b7fbd14b20d81400d92959b8017b8715258e1c
math7 <47001010+mat-h7@users.noreply.github.com>
2022-05-18 12:54:54 +0100

    Update s3Utils.js

--- src/s3Utils.js
+++ src/s3Utils.js
@@ -3,68 +3,6 @@ const AWS = require('aws-sdk');
 const debug = require("debug");
 const debugLog = debug("app:s3Utils");
 
-if (process.env.AWS_PROFILE_NAME) {
-	debugLog(`Using AWS Credentials from profile '${process.env.AWS_PROFILE_NAME}'`);
-
-	var credentials = new AWS.SharedIniFileCredentials({profile: process.env.AWS_PROFILE_NAME});
-	AWS.config.credentials = credentials;
-}
-
-debugLog(`Using AWS Access Key: ${AWS.config.credentials.accessKeyId}`);
-const s3Client = new AWS.S3({apiVersion: '2006-03-01'});
-
-
-const createBucket = (bucket, pathPrefix) => {
-	let prefix = (pathPrefix || "").trim();
-	if (prefix.length > 0 && !prefix.endsWith("/")) {
-		prefix = (prefix + "/");
-	}
-
-	return {
-		put: async (data, path, options={}) => {
-			let uploadParams = options || {};
-
-			uploadParams.Bucket = bucket;
-			uploadParams.Key = `${prefix}${path}`;
-			uploadParams.Body = data;
-				
-			await s3Client.putObject(uploadParams).promise();
-		},
-
-		get: async (path) => {
-			var getParams = {
-				Bucket: bucket,
-				Key: `${prefix}${path}`,
-			};
-			
-			try {
-				const s3Response = await s3Client.getObject(getParams).promise();
-
-				return s3Response.Body;
-
-			} catch (e) {
-				if (e.code == "NoSuchKey") {
-					return null;
-
-				} else {
-					throw e;
-				}
-			}
-		},
-
-		del: async (path) => {
-			var deleteParams = {
-				Bucket: bucket,
-				Key: `${prefix}${path}`,
-			};
-				
-			return await s3Client.deleteObject(deleteParams).promise();
-		}
-	};
-};
-
-
-
 module.exports = {
-	createBucket: createBucket
+	createBucket: null
 };
